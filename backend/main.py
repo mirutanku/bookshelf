d24ff926cd79b1ea80777d6c9379c7b80e65b83c
mirutanku <justin.charity@gmail.com>
2026-02-22 23:07:36 -0500

    Reassigned env var in place of harcoded URL

--- backend/main.py
+++ backend/main.py
@@ -20,7 +20,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
-        "https://bookshelf-two-tau.vercel.app",
+        os.getenv("FRONTEND_URL", ""),
     ],
     allow_credentials=True,
     allow_methods=["*"],
